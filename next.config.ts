1cb496bd174a3909d5d851478e77be0b32e03abe
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2025-04-04 23:57:42 +0600

    Image Issue Fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here */ 
+  basePath: '/land-unit-converter',
 };
 
 export default nextConfig;
